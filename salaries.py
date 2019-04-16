65d393a000a6524286f497bb05450feec2615a48
Bartłomiej Kurek <dev@metaceptron.com>
2019-04-16 20:17:10 +0200

    salaries

---
+++ salaries.py
@@ -0,0 +1,19 @@
+salaries = [1010, 3010, 2020, 5400, 7012, 8123, 4231, 5123]
+
+## Assignments:
+# 1. Print the list in ascending order, without modifying the list
+# 2. Print the list in descending order, without modifying the list
+# 3. Print the elements starting from the third one
+# 4. Print the elements backwards starting from the last one
+# 5. Print the sum of all the elements
+# 6. Print the sum of all even elements
+# 7. Print the max element
+# 8. Print the min element
+# 9. Print the number of unique elements
+# 10. Insert a number 6 at sixth position
+# 11. Remove all even elements
+# 13. Remove the last element
+# 14. Print the last element
+# 15. Add a list [8, 8] to the remaining list
+# 16. Print the length of the list
+
